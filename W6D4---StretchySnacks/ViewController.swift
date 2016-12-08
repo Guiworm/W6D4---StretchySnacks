f6a4051e9c4e5516c71206a81e475898df7a87e8
guiworm <dylan.mccrindle@gmail.com>
2016-12-08 10:39:37 -0500

    Initial Commit

---
+++ W6D4---StretchySnacks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  W6D4---StretchySnacks
+//
+//  Created by Dylan McCrindle on 2016-12-08.
+//  Copyright © 2016 Dylan-McCrindle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
